7fc2c9192bb8557e2f854e102f24c0d08ec48b0e
abstractafua <113399971+abstractafua@users.noreply.github.com>
2023-11-08 19:18:10 -0800

    Question 1

--- routes/listorder.js
+++ routes/listorder.js
@@ -11,8 +11,9 @@ app.get('/', function(req, res, next) {
     (async function(){
     /** Create connection, and validate that it connected successfully **/
     try {
-
         let pool = await sql.connect(dbConfig);
+
+    /** Write query to retrieve all order headers **/
 
         let sqlQuery = "SELECT ordersummary.orderID,ordersummary.orderDate, customer.customerId, customer.firstName, cutsomer.lastName, ordersummary.totalAmount" +
                          "FROM ordersummary O JOIN customer C ON O.customerID=C.customerID";
@@ -21,35 +22,6 @@ app.get('/', function(req, res, next) {
 
         let results = await pool.request()
             .query(sqlQuery);
-        
-        res.write("<table><tr><th>Order ID</th><th>Order Date</th><th>Customer ID</th><th>Customer Name</th><th>Total Amount</th></tr>");
-        for (let i = 0; i < results.recordset.length; i++) {
-            let result = results.recordset[i];
-            res.write("<tr><td>" + result.orderID + "</td><td>" + result.orderDate + "</td></tr>"+  result.customerID + "</td><td>" 
-            + result.firstName + result.lastName +  "</td></tr>"+ result.totalAmount + "</td></tr>");
-        }
-        res.write("</table>");
-
-        let results2 = await pool.request().query(sqlQuery2);
-
-
-
-
-        res.end();
-    } catch(err) {
-        console.dir(err);
-        res.write(JSON.stringify(err));
-        res.end();
-    }
-    
-
-    /**
-    Useful code for formatting currency:
-        let num = 2.87879778;
-        num = num.toFixed(2);
-    **/
-
-    /** Write query to retrieve all order headers **/
 
     /** For each order in the results
             Print out the order header information
@@ -59,8 +31,42 @@ app.get('/', function(req, res, next) {
                 Write out product information 
     **/
 
+    /**
+    Useful code for formatting currency:
+    /** */    
+
+        res.write("<table><tr><th>Order ID</th><th>Order Date</th><th>Customer ID</th><th>Customer Name</th><th>Total Amount</th></tr>");
+        for (let i = 0; i < results.recordset.length; i++) {
+             let result = results.recordset[i];
+             let num = result.totalAmount;
+             num = num.toFixed();
+
+            res.write("<tr><td>" + result.orderID + "</td><td>" + result.orderDate + "</td></tr>"+  result.customerID + "</td><td>" 
+            + result.firstName + result.lastName +  "</td></tr>"+ result.totalAmount + "</td></tr>");
+        }
+        res.write("</table>");
+
+        let results2 = await pool.request().query(sqlQuery2);
+
+        for (let i = 0; i < results2.recordset.length; i++) {
+            let results2 = results2.recordset[i];
+             let num2 = results2.totalAmount;
+             num2 = num.toFixed();
+            res.write("<tr><td>" + results2.productId + "</td><td>" + results2.quantity + "</td></tr>"+  results2.price );
+        }
+
+         res.write("</td><td></table>");
+
+        res.end();
+
+    } catch(err) {
+        console.dir(err);
+        res.write(JSON.stringify(err));
+        res.end();
+    }
+
     res.end();
-}});
+})});
 
 app.listen(3000)
 
