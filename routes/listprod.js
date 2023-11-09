531c84fa342cefbc2056d5296f0da902495721fe
abstractafua <113399971+abstractafua@users.noreply.github.com>
2023-11-09 08:37:37 -0800

    Second round changes

--- routes/listprod.js
+++ routes/listprod.js
@@ -3,11 +3,92 @@ const router = express.Router();
 const sql = require('mssql');
 
 router.get('/', function(req, res, next) {
+
+    // Rendering header
+app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.write("<title>YOUR NAME Grocery</title>")
+
+    res.write("<title>Search for the products you want to buy: </title>")
+    res.write("<title>Querying Using Node.js and Forms</title>");
+    res.write("<h1>Enter the name and/or department to search for:</h1>");
+    res.write('<form method="get" action="EmpQuery">');
+    res.write('Employee name: <input type="text" name="empname" size="25">');
+    res.write('Department: <input type="text" name="deptnum" size="5">');
+    res.write('<input type="submit" value="Submit">');
+    res.write('<input type="reset" value="Reset">');
+    res.write('</form>');
+})
+
+    res.setHeader('Content-Type', 'text/html');
+    
+
 
     // Get the product name to search for
+    (async function(){
+        /** Create connection, and validate that it connected successfully **/
+        try {
+            let pool = await sql.connect(dbConfig);
+    
+        /** Write query to retrieve all order headers **/
+            let sqlQuery =  " SELECT productName, productPrice FROM orderproduct";
+            let results = await pool.request()
+                .query(sqlQuery);
+
+            res.write("<h1>All Products</h1>");
+    
+        /** For each order in the results
+                Print out the order header information
+                Write a query to retrieve the products in the order
+    
+                For each product in the order
+                    Write out product information 
+        **/
+                    
+                    res.write("<tr><th>Product Name</th><th>Price</th><th>");
+    
+                //    res.write("<tr><th>Add to Cart</th><th></th><th>");
+        /**
+        Useful code for formatting currency:
+        /** */    
+            
+            for (let i = 0; i < results.recordset.length; i++) {
+                 let result = results.recordset[i];
+                 let num = result.totalAmount;
+                 num = num.toFixed();
+    
+                res.write("<tr><td>" + result.orderID + "</td><td>" + result.orderDate + "</td></tr>"+  result.customerID + "</td><td>" 
+                + result.firstName + result.lastName +  "</td></tr>"+ result.totalAmount + "</td></tr>");
+            }
+            res.write("</table>");
+    
+            let results2 = await pool.request().query(sqlQuery2);
+    
+            for (let i = 0; i < results2.recordset.length; i++) {
+                let results2 = results2.recordset[i];
+                 let num2 = results2.totalAmount;
+                 num2 = num.toFixed();
+                res.write("<tr><td>" + results2.productId + "</td><td>" + results2.quantity + "</td></tr>"+  results2.price );
+            }
+    
+             res.write("</td><td></table>");
+    
+            res.end();
+    
+        } catch(err) {
+            console.dir(err);
+            res.write(JSON.stringify(err));
+            res.end();
+        }
+    
+        res.end();
+    })});
+    
+    app.listen(3000)
+    
+    module.exports = router;
     let name = req.query.productName;
+
+    let sql1 = " SELECT * from "
     
     /** $name now contains the search string the user entered
      Use it to build a query and print out the results. **/
@@ -26,6 +107,8 @@ router.get('/', function(req, res, next) {
         let num = 2.89999;
         num = num.toFixed(2);
     **/
+
+
 
     res.end();
 });
